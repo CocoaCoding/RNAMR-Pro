2a72b7b5a8ff64d7095b3192f9104eb74f93fd02
Holger Hinzberg <holger@hinzberg.de>
2021-03-25 09:21:49 +0100

    Initial Commit

---
+++ RNAMR Pro/RNAMR_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  RNAMR_ProApp.swift
+//  RNAMR Pro
+//
+//  Created by Holger Hinzberg on 25.03.21.
+//
+
+import SwiftUI
+
+@main
+struct RNAMR_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
